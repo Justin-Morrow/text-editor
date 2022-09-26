c47a7a354b4921a5ddf9a58d3c5e8e8982f6a99d
Justin Morrow <morrowjustin19@gmail.com>
2022-09-25 19:44:00 -0700

    updating

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -11,20 +11,64 @@ module.exports = () => {
     mode: 'development',
     entry: {
       main: './src/js/index.js',
-      install: './src/js/install.js'
+      install: './src/js/install.js',
+      editor: './src/js/editor.js',
     },
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
-    ],
-
-    module: {
-      rules: [
-        
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      title: 'Just Another Text Editor',
+    }),
+    new InjectManifest({
+      swSrc: './src-sw.js',
+      swDest: 'src-sw.js',
+    }),
+    new WebpackPwaManifest({
+      fingerprints: false,
+      inject: true,
+      name: 'text-editor',
+      short_name: 'JATE',
+      description: 'Just Another Text Editor',
+      background_color: '#282A36',
+      theme_color: '#6c81f7',
+      id: '/',
+      start_url: '/',
+      publicPath: '/',
+      icons: [
+        {
+          src: path.resolve('src/images/logo.png'),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join('assets', 'icons'),
+        },
       ],
+    }),
+  ],
+// Css loaders and babel added
+module: {
+  rules: [
+    {
+      test: /\.css$/i,
+      use: ['style-loader', 'css-loader'],
     },
-  };
+    {
+      test: /\.m?js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env'],
+          plugins: [
+            '@babel/plugin-proposal-object-rest-spread',
+            '@babel/transform-runtime',
+          ],
+        },
+      },
+    },
+  ],
+},
 };
+}; 
