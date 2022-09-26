ee68277e8a2534a658f6350121231085285924bf
Justin Morrow <morrowjustin19@gmail.com>
2022-09-25 21:39:06 -0700

    working on install button and displaying header

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,4 +1,4 @@
-const butInstall = document.getElementById('buttonInstall');
+const installBtn = document.getElementById('installBtn');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
@@ -8,7 +8,7 @@ window.addEventListener('beforeinstallprompt', (event) => {
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {
+installBtn.addEventListener('click', async () => {
     const promptEvent = window.deferredPrompt;
     if (!promptEvent) {
     return;
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,9 +3,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
     mode: 'development',
@@ -19,56 +16,57 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-    new HtmlWebpackPlugin({
-      template: './index.html',
-      title: 'Just Another Text Editor',
-    }),
-    new InjectManifest({
-      swSrc: './src-sw.js',
-      swDest: 'src-sw.js',
-    }),
-    new WebpackPwaManifest({
-      fingerprints: false,
-      inject: true,
-      name: 'text-editor',
-      short_name: 'JATE',
-      description: 'Just Another Text Editor',
-      background_color: '#282A36',
-      theme_color: '#6c81f7',
-      id: '/',
-      start_url: '/',
-      publicPath: '/',
-      icons: [
+      // workbox plugins for service worker and manifest file
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Just Another Text Editor',
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'text-editor',
+        short_name: 'JATE',
+        description: 'Just Another Text Editor',
+        background_color: '#282A36',
+        theme_color: '#6c81f7',
+        id: '/',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
+    ],
+    // CSS loaders and babel added
+    module: {
+      rules: [
         {
-          src: path.resolve('src/images/logo.png'),
-          sizes: [96, 128, 192, 256, 384, 512],
-          destination: path.join('assets', 'icons'),
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: [
+                '@babel/plugin-proposal-object-rest-spread',
+                '@babel/transform-runtime',
+              ],
+            },
+          },
         },
       ],
-    }),
-  ],
-// Css loaders and babel added
-module: {
-  rules: [
-    {
-      test: /\.css$/i,
-      use: ['style-loader', 'css-loader'],
     },
-    {
-      test: /\.m?js$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-env'],
-          plugins: [
-            '@babel/plugin-proposal-object-rest-spread',
-            '@babel/transform-runtime',
-          ],
-        },
-      },
-    },
-  ],
-},
+  };
 };
-}; 
