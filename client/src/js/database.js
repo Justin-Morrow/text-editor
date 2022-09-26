489439cc2f7b219e408b3bfd5cc87f8d667e4bc1
Justin Morrow <morrowjustin19@gmail.com>
2022-09-25 18:38:35 -0700

    updating

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,30 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('put data into JATEdb');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({ id: 1, value: content });
+  const result = await request;
+  console.log('data saved to the JATEdb', result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getAllDb = async () => {
+  console.log('get data from JATEdb');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.getAll();
+  const result = await request;
+  console.log('data received from JATEdb', result);
+
+  // checks for and returns stored notes, if they exist
+  if (result[0]) {
+    return result[0].value;
+  }
+};
 
 initdb();
